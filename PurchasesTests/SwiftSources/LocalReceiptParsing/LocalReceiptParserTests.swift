d21ee7263118128ed34bc4577860894f367e80da
Andy Boedo <andresboedo@gmail.com>
2020-07-22 15:55:35 -0300

    added files for local testing

--- PurchasesTests/SwiftSources/LocalReceiptParsing/LocalReceiptParserTests.swift
+++ PurchasesTests/SwiftSources/LocalReceiptParsing/LocalReceiptParserTests.swift
@@ -11,7 +11,36 @@ import XCTest
 @testable import Purchases
 
 class LocalReceiptParserTests: XCTestCase {
-    func testCanInitialize() {
-        expect { LocalReceiptParser() } .notTo(raiseException())
+    
+    func testParseReceiptWithTPInAppReceipt() {
+        let receiptData = sampleReceiptData()
+        do {
+            let parsedReceipt = try InAppReceipt(receiptData: receiptData)
+            print(parsedReceipt)
+        }
+        catch let error {
+            fatalError("couldn't parse receipt. error: \(error.localizedDescription)")
+        }
+    }
+}
+
+private extension LocalReceiptParserTests {
+    
+    func sampleReceiptData() -> Data {
+        let receiptText = readFile(named: "base64encodedreceipt")
+        guard let receiptData = Data(base64Encoded: receiptText) else { fatalError("couldn't decode file") }
+        return receiptData
+    }
+    
+    func readFile(named filename: String) -> String {
+        guard let pathString = Bundle(for: type(of: self)).path(forResource: filename, ofType: "txt") else {
+            fatalError("\(filename) not found")
+        }
+        do {
+            return try String(contentsOfFile: pathString, encoding: String.Encoding.utf8)
+        }
+        catch let error {
+            fatalError("couldn't read file named \(filename). Error: \(error.localizedDescription)")
+        }
     }
 }
