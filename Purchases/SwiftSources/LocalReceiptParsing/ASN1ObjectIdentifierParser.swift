a61f380a5e39a14a355f760fbd63c7c3dac0c1e7
Andy Boedo <andresboedo@gmail.com>
2020-07-28 19:38:45 -0300

    cleanup, separated objectIdentifierParser

---
+++ Purchases/SwiftSources/LocalReceiptParsing/ASN1ObjectIdentifierParser.swift
@@ -0,0 +1,58 @@
+//
+// Created by Andrés Boedo on 7/28/20.
+// Copyright (c) 2020 Purchases. All rights reserved.
+//
+
+import Foundation
+
+enum ASN1ObjectIdentifier: String {
+    case data = "1.2.840.113549.1.7.1"
+    case signedData = "1.2.840.113549.1.7.2"
+    case envelopedData = "1.2.840.113549.1.7.3"
+    case signedAndEnvelopedData = "1.2.840.113549.1.7.4"
+    case digestedData = "1.2.840.113549.1.7.5"
+    case encryptedData = "1.2.840.113549.1.7.6"
+}
+
+class ASN1ObjectIdentifierParser {
+    func extractObjectIdentifier(payload: ArraySlice<UInt8>) -> ASN1ObjectIdentifier? {
+        guard let firstByte = payload.first else { fatalError("invalid object identifier") }
+
+        var oidBytes: [UInt] = []
+        oidBytes.append(UInt(firstByte / 40))
+        oidBytes.append(UInt(firstByte % 40))
+
+        let trailingPayload = payload.dropFirst()
+        var currentValue: UInt = 0
+        var isShortLength = false
+        var isAppendingToLongValue = false
+        for byte in trailingPayload {
+            isShortLength = byte.bitAtIndex(0) == 0
+            let byteValue = UInt(byte.valueInRange(from: 1, to: 7))
+
+            if isAppendingToLongValue {
+                currentValue = (currentValue << 7) | byteValue
+                if isShortLength {
+                    oidBytes.append(currentValue)
+                    isAppendingToLongValue = false
+                    currentValue = 0
+                } else {
+                    isAppendingToLongValue = true
+                }
+            } else {
+                if isShortLength {
+                    oidBytes.append(byteValue)
+                    isAppendingToLongValue = false
+                    currentValue = 0
+                } else {
+                    currentValue = byteValue
+                    isAppendingToLongValue = true
+                }
+            }
+        }
+
+        let oidString = oidBytes.map { String($0) }
+                                .joined(separator: ".")
+        return ASN1ObjectIdentifier(rawValue: oidString)
+    }
+}
--- Purchases/SwiftSources/LocalReceiptParsing/ReceiptParser.swift
+++ Purchases/SwiftSources/LocalReceiptParsing/ReceiptParser.swift
@@ -9,13 +9,19 @@
 import Foundation
 
 struct ReceiptParser {
+    let objectIdentifierParser: ASN1ObjectIdentifierParser
+
+    init() {
+        self.objectIdentifierParser = ASN1ObjectIdentifierParser()
+    }
+
     func extract(from data: Data) {
         let intData = [UInt8](data)
 
         let asn1Container = extractASN1(withPayload: ArraySlice(intData))
     }
 
-    func extractASN1(withPayload payload: ArraySlice<UInt8>) -> ASN1Container{
+    func extractASN1(withPayload payload: ArraySlice<UInt8>) -> ASN1Container {
         guard payload.count >= 2,
             let firstByte = payload.first else { fatalError("data format invalid") }
         let containerClass = extractClass(byte: firstByte)
@@ -32,7 +38,9 @@ struct ReceiptParser {
                 internalContainers.append(internalContainer)
                 currentPayload = currentPayload.dropFirst(internalContainer.totalBytes)
                 if internalContainer.containerType == .objectIdentifier {
-                    guard let objectIdentifier = extractObjectIdentifier(payload: internalContainer.internalPayload) else {
+                    guard let objectIdentifier = objectIdentifierParser.extractObjectIdentifier(payload:
+                                                                                                internalContainer.internalPayload)
+                        else {
                         break
                     }
                     switch objectIdentifier {
@@ -73,11 +81,7 @@ struct ReceiptParser {
             return
         }
 
-//        print("found type: \(nonOptionalType)")
-//        print("found version: \(version)")
-
         extractReceiptAttributeValue(fromContainer: valueContainer, withType: nonOptionalType)
-//        print("found value: \(value)")
     }
 
     func extractReceiptAttributeValue(fromContainer container: ASN1Container,
@@ -145,56 +149,6 @@ struct ReceiptParser {
             let internalContainer = extractASN1(withPayload: container.internalPayload)
             return String(bytes: internalContainer.internalPayload, encoding: .ascii)!
         }
-    }
-
-    func extractObjectIdentifier(payload: ArraySlice<UInt8>) -> ASN1ObjectIdentifier? {
-        guard let firstByte = payload.first else { fatalError("invalid object identifier") }
-
-        var oidBytes: [UInt] = []
-        oidBytes.append(UInt(firstByte / 40))
-        oidBytes.append(UInt(firstByte % 40))
-
-        let trailingPayload = payload.dropFirst()
-        var currentValue: UInt = 0
-        var isShortLength = false
-        var isAppendingToLongValue = false
-        for byte in trailingPayload {
-            isShortLength = byte.bitAtIndex(0) == 0
-            let byteValue = UInt(byte.valueInRange(from: 1, to: 7))
-
-            if isAppendingToLongValue {
-                currentValue = (currentValue << 7) | byteValue
-                if isShortLength {
-                    oidBytes.append(currentValue)
-                    isAppendingToLongValue = false
-                    currentValue = 0
-                } else {
-                    isAppendingToLongValue = true
-                }
-            } else {
-                if isShortLength {
-                    oidBytes.append(byteValue)
-                    isAppendingToLongValue = false
-                    currentValue = 0
-                } else {
-                    currentValue = byteValue
-                    isAppendingToLongValue = true
-                }
-            }
-        }
-
-        let oidString = oidBytes.map { String($0) }
-                                .joined(separator: ".")
-        return ASN1ObjectIdentifier(rawValue: oidString)
-    }
-
-    enum ASN1ObjectIdentifier: String {
-        case data = "1.2.840.113549.1.7.1"
-        case signedData = "1.2.840.113549.1.7.2"
-        case envelopedData = "1.2.840.113549.1.7.3"
-        case signedAndEnvelopedData = "1.2.840.113549.1.7.4"
-        case digestedData = "1.2.840.113549.1.7.5"
-        case encryptedData = "1.2.840.113549.1.7.6"
     }
 
     func extractClass(byte: UInt8) -> ASN1Class {
